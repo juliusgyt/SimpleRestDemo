270d7bf312b410ba4a2463310bcee4dc0e2d3a16
Julius <jvarela57@yahoo.es>
2019-08-22 14:15:16 -0400

    Se restaura el consumo usando http y la url simplerestdemo.herokuapp.com en reemplazo de localhost:8080

--- src/main/resources/static/js/angular2.js
+++ src/main/resources/static/js/angular2.js
@@ -1,8 +1,11 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
 //	alert("Invocando el servicio REST http://localhost:8080/clientes/3 con AngularJS");
+	url = "https://simplerestdemo.herokuapp.com/clientes/3";
+	//url = "http://localhost:8080/clientes/3";
 //	$http.get('http://localhost:8080/clientes/3').
-	$http.get('https://simplerestdemo.herokuapp.com/clientes/3').
+//	$http.get('https://simplerestdemo.herokuapp.com/clientes/3').
+	$http.get(url).
         then(function(response) {
             $scope.greeting = response.data;
         });
--- src/main/resources/static/js/jquery2.js
+++ src/main/resources/static/js/jquery2.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 //	alert("Invocando el servicio REST http://localhost:8080/clientes/3 con jQuery");
+//	http://simplerestdemo.herokuapp.com/clientes/3
     $.ajax({
-        url: "https://simplerestdemo.herokuapp.com/clientes/3"
+        url: "http://simplerestdemo.herokuapp.com/clientes/3"
     }).then(function(data) {
        $('.greeting-idCliente').append(data.idCliente);
        $('.greeting-nombre').append(data.nombre);
