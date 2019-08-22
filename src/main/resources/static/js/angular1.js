87fd671472095b1f763cb2df96f9e9f81d545fe2
Julius <jvarela57@yahoo.es>
2019-08-22 12:08:36 -0400

    Reemplazar https por http completamente

--- src/main/resources/static/js/angular1.js
+++ src/main/resources/static/js/angular1.js
@@ -1,7 +1,7 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
 //	alert("Invocando el servicio REST http://rest-service.guides.spring.io/greeting con AngularJS");
-	$http.get('http://rest-service.guides.spring.io/greeting').
+	$https.get('https://rest-service.guides.spring.io/greeting').
         then(function(response) {
             $scope.greeting = response.data;
         });
--- src/main/resources/static/js/jquery1.js
+++ src/main/resources/static/js/jquery1.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 //	alert("Invocando el servicio REST http://rest-service.guides.spring.io/greeting con jQuery");
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "https://rest-service.guides.spring.io/greeting"
     }).then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
