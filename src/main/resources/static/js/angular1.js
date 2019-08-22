4d0259d1f36299ea54139a08e5b807d94e65926e
Julius <jvarela57@yahoo.es>
2019-08-22 12:24:41 -0400

    se corrige el uso del objeto $https por $http en consumo con angularJS

--- src/main/resources/static/js/angular1.js
+++ src/main/resources/static/js/angular1.js
@@ -1,7 +1,7 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
 //	alert("Invocando el servicio REST http://rest-service.guides.spring.io/greeting con AngularJS");
-	$https.get('https://rest-service.guides.spring.io/greeting').
+	$http.get('https://rest-service.guides.spring.io/greeting').
         then(function(response) {
             $scope.greeting = response.data;
         });
