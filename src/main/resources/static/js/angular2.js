b68098cdccbea52c45231cc70edaa4ae567b7282
Julius <jvarela57@yahoo.es>
2019-08-22 10:13:54 -0400

    invocación del servicio REST con https en vez de http

--- src/main/resources/static/js/angular2.js
+++ src/main/resources/static/js/angular2.js
@@ -2,7 +2,7 @@ angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
 //	alert("Invocando el servicio REST http://localhost:8080/clientes/3 con AngularJS");
 //	$http.get('http://localhost:9898/clientes/3').
-	$http.get('http://simplerestdemo.herokuapp.com/clientes/3').
+	$http.get('https://simplerestdemo.herokuapp.com/clientes/3').
         then(function(response) {
             $scope.greeting = response.data;
         });
--- src/main/resources/static/js/jquery2.js
+++ src/main/resources/static/js/jquery2.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 //	alert("Invocando el servicio REST http://localhost:8080/clientes/3 con jQuery");
     $.ajax({
-        url: "http://simplerestdemo.herokuapp.com/clientes/3"
+        url: "https://simplerestdemo.herokuapp.com/clientes/3"
     }).then(function(data) {
        $('.greeting-idCliente').append(data.idCliente);
        $('.greeting-nombre').append(data.nombre);
